3b5cf19833602b924ad487f74c7e8f1b38961f8f
kazuki aruga <kazuki.aruga@gmail.com>
2016-11-26 23:24:18 +0900

    新規登録

---
+++ src/main/java/io/github/kazuki_aruga/findata_import/App.java
@@ -0,0 +1,13 @@
+package io.github.kazuki_aruga.findata_import;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
